282f697fdfd30ec18cd0c2c7e5c08dc09a3a700d
Gabriel-brogni-bento <32969979+Gabriel-brogni-bento@users.noreply.github.com>
2019-04-17 20:15:23 -0300

    Commit inicial

---
+++ RealidadeVirtual/Assets/RealidadeVirtual/Script/Leap_Paint/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject cubeMenu;
+    public GameObject cubeCores;
+    public bool control = true;
+
+    public TrailRenderer trailPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailPrefab.startColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActive()
+    {
+        cubeMenu.SetActive(control);
+        cubeCores.SetActive(false);
+        control = !control;
+    }
+
+    public void ActivateColor()
+    {
+        cubeMenu.SetActive(false);
+        cubeCores.SetActive(true);
+    }
+
+    public void SetRedColor()
+    {
+        print("Vermelho");
+        trailPrefab.startColor = Color.red;
+        trailPrefab.endColor = Color.red;
+    }
+
+    public void SetGreenColor()
+    {
+        print("Verde");
+        trailPrefab.startColor = Color.green;
+        trailPrefab.endColor = Color.green;
+    }
+
+    public void SetBlueColor()
+    {
+        print("Azul");
+        trailPrefab.startColor = Color.blue;
+        trailPrefab.endColor = Color.blue;
+    }
+
+    public void SetTrailWidth()
+    {
+        trailPrefab.widthMultiplier = 1;
+    }
+}
---
+++ RealidadeVirtual/Assets/RealidadeVirtual/Script/Pc_Write/MoveCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject CameraView;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveCameraRigth()
+    {
+        CameraView.transform.position = new Vector3(CameraView.transform.position.x + 750, CameraView.transform.position.y,CameraView.transform.position.z);
+    }
+
+    public void MoveCameraLeft()
+    {
+        CameraView.transform.position = new Vector3(CameraView.transform.position.x - 750, CameraView.transform.position.y, CameraView.transform.position.z);
+    }
+}
